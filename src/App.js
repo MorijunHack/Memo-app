556fb5b658af4e1a69d7b87cb4795c4cde68eff2
Jun Morimoto <morijun@junnoMacBook-Air.local>
2019-08-27 11:02:59 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import Memo from './memo/Memo';
+import AddForm from './forms/AddForm';
+import FindForm from './forms/FindForm';
+import DelForm from './forms/DelForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  td = {
+    width: "250px"
+  }
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}><FindForm /></td>
+              <td style={this.td}><DelForm /></td>
+            </tr>
+          </tbody>
+        </table>
+        <Memo />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
